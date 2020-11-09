08092774edf0753f5757a4258f128cc12f5f7313
JordanChat <69655794+JordanChat@users.noreply.github.com>
2020-11-08 21:37:01 -0500

    update create tables

--- EmployeeSQL/Creating_Tables.sql
+++ EmployeeSQL/Creating_Tables.sql
@@ -5,58 +5,42 @@ DROP TABLE if exists employees;
 DROP TABLE if exists salaries;
 DROP TABLE if exists titles;
 
-CREATE TABLE "departments" (
-    "dept_no" VARCHAR(10)   NOT NULL,
-    "dept_name" VARCHAR(40)   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
+CREATE TABLE departments (
+    dept_no VARCHAR(30) PRIMARY KEY  NOT NULL,
+    dept_name VARCHAR(40)   NOT NULL
 );
 
-CREATE TABLE "dept_emp" (
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY  NOT NULL,
+    title_id VARCHAR(30)   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR(50)   NOT NULL,
+    last_name VARCHAR(50)   NOT NULL,
+    sex VARCHAR(30)   NOT NULL,
+    hire_date DATE   NOT NULL
+);
+
+CREATE TABLE dept_emp (
     "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(10)   NOT NULL
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    "dept_no" VARCHAR(30)   NOT NULL,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR(10)   NOT NULL,
-    "emp_no" INT   NOT NULL
-);
-
-CREATE TABLE "employees" (
+    "dept_no" VARCHAR(30)   NOT NULL,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     "emp_no" INT   NOT NULL,
-    "title_id" VARCHAR(10)   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR(15)   NOT NULL,
-    "last_name" VARCHAR(15)   NOT NULL,
-    "sex" VARCHAR(15)   NOT NULL,
-    "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     "salary" INT   NOT NULL
 );
 
 CREATE TABLE "titles" (
-    "title_id" VARCHAR(10)   NOT NULL,
-    "title" VARCHAR(10)   NOT NULL
+    "title_id" VARCHAR(30)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL
 );
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
